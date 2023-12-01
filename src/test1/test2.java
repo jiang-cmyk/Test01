ff1c591b9cc3c4cabe1f4222d352113d76c19782
jiang <2630353402@qq.com>
2023-12-01 10:27:41 +0800

    00

--- src/test1/test2.java
+++ src/test1/test2.java
@@ -9,10 +9,10 @@ public class test2 {
 
 	public static void main(String[] args) throws Exception {
 
-		String Input = "C:\\Users\\ABC\\Desktop\\2022-10\\data20221024";
+		String Input = "C:\\Users\\ABC\\Documents\\WeChat Files\\wxid_pueqh3fdguqv22\\FileStorage\\File\\2023-11\\月底统计20231127";
 		String Output = "C:/Users/ABC/Desktop/Output";
-		Date stdate =new SimpleDateFormat("yyyy-MM-dd").parse("2022-09-25");
-		Date endate =new SimpleDateFormat("yyyy-MM-dd").parse("2022-10-24");
+		Date stdate =new SimpleDateFormat("yyyy-MM-dd").parse("2023-10-25");
+		Date endate =new SimpleDateFormat("yyyy-MM-dd").parse("2023-11-24");
 
 		caculate caculate = new caculate();
 		caculate.run(Input, Output, stdate, endate);
